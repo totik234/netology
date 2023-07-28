ff4cbc383fa293318adda2346fe74cc6d3c48da6
Denis Shulc <dshulc@secureducation.ru>
2023-07-28 09:47:43 +0300

    fix lesson 7.2

--- lesson_7.2/src/for_each-vm.tf
+++ lesson_7.2/src/for_each-vm.tf
@@ -1,21 +1,22 @@
 resource "yandex_compute_instance" "main_and_replica" {
   depends_on = [yandex_compute_instance.count_vm]
-  for_each =toset(["0","1"])
 
-  name        = var.for_each_vm[each.key].vm_name
+  for_each = {for vm in var.for_each_vm: vm.vm_name => vm}
+
+  name        = each.value.vm_name
   platform_id = var.platform_id
 
   resources {
-    cores         = var.for_each_vm[each.key].cpu
-    memory        = var.for_each_vm[each.key].ram
-    core_fraction = var.for_each_vm[each.key].core_fraction
+    cores         = each.value.cpu
+    memory        = each.value.ram
+    core_fraction = each.value.core_fraction
   }
 
   boot_disk {
     initialize_params {
       image_id = data.yandex_compute_image.ubuntu-2004-lts.image_id
       type     = var.boot_disk_config.type[0]
-      size     = var.for_each_vm[each.key].disk
+      size     = each.value.disk
     }
   }
 
