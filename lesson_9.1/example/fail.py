478491963a6ac5fd5557881422ed48349dae6c4a
Denis Shulc <dshulc@secureducation.ru>
2024-03-26 21:43:49 +0300

    lesson 9.1

---
+++ lesson_9.1/example/fail.py
@@ -0,0 +1,11 @@
+def increment(index=0):
+    index =+ 1
+    return index
+def get_square(numb):
+    return numb*numb
+def print_numb(numb):
+    print("Number is {}".format(numb))
+
+while (index < 10):
+    index = increment(index)
+    print(get_square(index))
